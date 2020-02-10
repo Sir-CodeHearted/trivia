dad9b4f135f6948c300104c117533cf17583b194
LLUXX <kentastooth@gmail.com>
2020-02-10 18:17:02 -0500

    jamie foxx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import {LuxComponent} from './LuxComponent';
+import {buildFirebase} from '../clients/firebase';
 
 // import components
 
 class App extends Component {
+ constructor(props) {
+   super(props);
+   this.gonget();
+ }
+ gonget (){
+ var database = buildFirebase();
+ var databaseRef = database.ref("/questions");
+ databaseRef.once("value").then(function(data){
+   const questions = data.val();
+   console.log(questions) 
+ });
+
+ }
+
+
+
 
   render() {
     return (
@@ -11,6 +28,7 @@ class App extends Component {
      <LuxComponent questionText= 'how many states are there' answers = {['50','52', '48', '49']}> 
      </LuxComponent> 
      </div>
+     
     );
   }
 }
